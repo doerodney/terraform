98e2302c4cb11b0806dc11ebb18669acc137b425
aru2 <rod.b.doe@nordstrom.com>
2019-07-02 15:20:10 -0700

    Added info document to version control.

--- training/example.tf
+++ training/example.tf
@@ -1,13 +1,35 @@
 provider "aws" {
   profile = "default"
-  region = "us-west-2"
+  region  = "us-west-2"
 }
 
 resource "aws_instance" "example" {
-  ami = "ami-b307ffd3"
-  instance_type = "t2.micro" 
-  subnet_id = "subnet-f8caf78e" 
+  ami           = "ami-0f2176987ee50226e"
+  instance_type = "t2.micro"
+  key_name      = "inftools_sandbox"
+  subnet_id     = "subnet-f8caf78e"
   tags = {
     Name = "Created by doer using Terraform"
-  }     
+  }
+
+  user_data = <<-EOF
+    #!/bin/bash
+    echo "Hello, World" > index.html
+    nohup busybox httpd -f -p 8080 &
+    EOF     
+
+  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+}
+
+resource "aws_security_group" "instance" {
+  description = "Allow tcp traffic on port 8080"
+  name = "terraform-example-instance"
+  vpc_id = "vpc-d7b04eb0"
+
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
